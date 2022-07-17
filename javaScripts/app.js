473351c4cc27c7970905a37f12ed1578cf2f28e4
Huthaifa Altiti <huthaefaaltete094@gmail.com>
2022-07-17 16:01:06 +0300

    End CSS code  and editing JS code, make it OOP.

--- javaScripts/app.js
+++ javaScripts/app.js
@@ -1,3 +1,5 @@
+// Variables
+
 let inputX = document.querySelector(".input-x");
 let inputY = document.querySelector(".input-y");
 let inputZ = document.querySelector(".input-z");
@@ -10,33 +12,39 @@ let outputBox = document.querySelector(".cube");
 let btnReset = document.querySelector(".btn-reset");
 let btnCopy = document.querySelector(".btn-copy");
 
+//Functions
 
+function rotateXYZ() {
+  return `rotateX(${inputX.value}deg) rotateY(${inputY.value}deg) rotateZ(${inputZ.value}deg)`;
+}
+
+// Main DOMs
 
 inputX.addEventListener("input", function () {
-  resultX.innerText = inputX.value + " Deg";
+  resultX.innerText = inputX.value + " deg";
 
-  outputBox.style.transform = `rotateX(${inputX.value}deg) rotateY(${inputY.value}deg) rotateZ(${inputZ.value}deg)`;
+  outputBox.style.transform = rotateXYZ();
 });
 
 inputY.addEventListener("input", function () {
-  resultY.innerText = inputY.value + " Deg";
+  resultY.innerText = inputY.value + " deg";
 
-  outputBox.style.transform = `rotateX(${inputX.value}deg) rotateY(${inputY.value}deg) rotateZ(${inputZ.value}deg)`;
+  outputBox.style.transform = rotateXYZ();
 });
 
 inputZ.addEventListener("input", function () {
-  resultZ.innerText = inputZ.value + " Deg";
+  resultZ.innerText = inputZ.value + " deg";
 
-  outputBox.style.transform = `rotateX(${inputX.value}deg) rotateY(${inputY.value}deg) rotateZ(${inputZ.value}deg)`;
+  outputBox.style.transform = rotateXYZ();
 });
 
 btnReset.addEventListener("click", function () {
   inputX.value = inputY.value = inputZ.value = 0;
 
-  outputBox.style.transform = `rotateX(${inputX.value}deg) rotateY(${inputY.value}deg) rotateZ(${inputZ.value}deg)`;
+  outputBox.style.transform = rotateXYZ();
 });
 
-btnCopy.addEventListener('click', function() {
-
-  console.log('transform', outputBox.style.transform);
-}) 
+// copy to console
+btnCopy.addEventListener("click", function () {
+  console.log("transform", outputBox.style.transform);
+});
